3d8dcaa126a6017728560929e728feb017004a8e
programmatoroSeduto <francesc.ganci@gmail.com>
2021-09-24 14:51:16 +0000

    small updates on state_machine.cpp; cancellation is still missimg

--- src/state_machine.cpp
+++ src/state_machine.cpp
@@ -2,17 +2,26 @@
 #include "rt2_assignment1/Command.h"
 #include "rt2_assignment1/Position.h"
 #include "rt2_assignment1/RandomPosition.h"
-#include "rt2_assignment1/GoToPoint.h"
+#include "rt2_assignment1/GoToPointAction.h"
 #include "actionlib/client/simple_action_client.h"
 #include "actionlib/client/terminal_state.h"
 
 bool start = false;
 
-bool user_interface(rt2_assignment1::Command::Request &req, rt2_assignment1::Command::Response &res){
-    if (req.command == "start"){
+bool user_interface(rt2_assignment1::Command::Request &req, rt2_assignment1::Command::Response &res)
+{
+    if (req.command == "start")
+    {
     	start = true;
+    	std::cout << "[state_machine] START command received. " << std::endl;
     }
-    else {
+    else 
+    {
+    	if ( start )
+			std::cout << "[state_machine] STOP command received. " << std::endl;
+		else
+			std::cout << "[state_machine] state machine already stopped. " << std::endl;
+		
     	start = false;
     }
     return true;
@@ -26,8 +35,8 @@ int main(int argc, char **argv)
    ros::ServiceServer service= n.advertiseService("/user_interface", user_interface);
    ros::ServiceClient client_rp = n.serviceClient<rt2_assignment1::RandomPosition>("/position_server");
    ros::ServiceClient client_p = n.serviceClient<rt2_assignment1::Position>("/go_to_point");
-   actionlib::SimpleActionClient<rt2_assignment1::GoToPoint> ac( "go_to_point", True );
-   ac.waitForServer( )
+   actionlib::SimpleActionClient<rt2_assignment1::GoToPointAction> ac( "go_to_point", true );
+   ac.waitForServer( );
    
    rt2_assignment1::RandomPosition rp;
    rp.request.x_max = 5.0;
@@ -38,7 +47,8 @@ int main(int argc, char **argv)
    
    while(ros::ok()){
    	ros::spinOnce();
-   	if (start){
+   	if (start)
+   	{
 		// ask for a random target
    		client_rp.call(rp);
    		
@@ -50,6 +60,7 @@ int main(int argc, char **argv)
    		goal.x = rp.response.x;
    		goal.y = rp.response.y;
    		goal.theta = rp.response.theta;
+   		cout << "NEXT TARGET (" << goal.x << ", " << goal.y << ", " << goal.theta << ")" << std::endl;
    		// std::cout << "\nGoing to the position: x= " << p.request.x << " y= " <<p.request.y << " theta = " <<p.request.theta << std::endl;
    		//client_p.call(p);
    		

3c9103c353489fe5d2a2cc8edc9d0d0b057e1be6
programmatoroSeduto <francesc.ganci@gmail.com>
2022-04-21 12:49:25 +0000

    scripts, minor changes

--- shell/shell_build_ws_only.sh
+++ shell/shell_build_ws_only.sh
@@ -4,6 +4,6 @@
 
 rm -rf ../logs
 mkdir ../logs
-terminator --working-directory=../ws_ros1 --title="ROS1 build" -e "source ./build.sh > ../logs/ws_ros1_build.log"
-terminator --working-directory=../ws_ros2 --title="ROS2 build" -e "source ./build.sh > ../logs/ws_ros2_build.log"
+terminator --title="ROS1 build" --working-directory="/root/rt2_assignment_1/ws_ros1" -e "source ./build.sh > ../logs/ws_ros1_build.log"
+terminator --title="ROS2 build" --working-directory="/root/rt2_assignment_1/ws_ros2" -e "source ./build.sh > ../logs/ws_ros2_build.log"
 # terminator --working-directory=../ws_bridge --title="BRIDGE build" -e "source ./build_bridge_base.sh > ../logs/ws_bridge_build.log"
